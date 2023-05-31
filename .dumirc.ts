247743ba9ba45d07544a60eab1185876f783ec7f
youngjuning <1003719811@qq.com>
2023-05-31 23:19:34 +0800

    add monetag

--- .dumirc.ts
+++ .dumirc.ts
@@ -33,4 +33,11 @@ export default defineConfig({
   headScripts: [
     {src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7029815294762181', async: true, crossorigin: 'anonymous'}
   ],
+  scripts: [
+    {
+      src: 'https://uwoaptee.com/pfe/current/tag.min.js?z=6004204',
+      async: true,
+      "data-cfasync": false,
+    }
+  ]
 });
