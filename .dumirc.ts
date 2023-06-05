1f34572e1147d6be840c7511832a6f918a572439
youngjuning <1003719811@qq.com>
2023-06-05 11:29:18 +0800

    chore: 换成弹出式广告，侵入式不大

--- .dumirc.ts
+++ .dumirc.ts
@@ -34,10 +34,6 @@ export default defineConfig({
     {src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7029815294762181', async: true, crossorigin: 'anonymous'}
   ],
   scripts: [
-    {
-      src: 'https://uwoaptee.com/pfe/current/tag.min.js?z=6004204',
-      async: true,
-      "data-cfasync": false,
-    }
+    `(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('belickitungchan.com',6018475,document.createElement('script'))`
   ]
 });
