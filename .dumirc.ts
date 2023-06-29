0e140cab74c22937573e1115c6598aab528fc718
紫竹 <luozhu2021@gmail.com>
2023-06-29 15:53:42 +0800

    Update .dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -31,6 +31,6 @@ export default defineConfig({
   exportStatic: {},
   ...(process.env.NODE_ENV === 'development' ? {} : { ssr: {} }),
   headScripts: [
-    // {src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7029815294762181', async: true, crossorigin: 'anonymous'}
+    {src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7029815294762181', async: true, crossorigin: 'anonymous'}
   ],
 });
