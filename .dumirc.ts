58a2c659fa58c8b57661c07df773a061536f5cf9
洛竹 <luozhu2021@gmail.com>
2023-07-09 22:40:36 +0800

    add footer

--- .dumirc.ts
+++ .dumirc.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       twitter: 'https://twitter.com/luozhu2021'
     },
     hd: { rules: [] },
-    footer: 'Made with ❤️ by <a href="https://youngjuning.js.org/zizhudocs" target="_blank" nofollow>紫竹翻译计划</a>'
+    footer: '<a href="/sitemap.xml" target="_blank">站点地图</a> | <a href="https://youngjuning.js.org/zizhudocs" target="_blank">紫竹翻译计划</a> | <a href="https://t.zsxq.com/0fDyODCQs" target="_blank">紫竹赚美刀</a><br/>Made with ❤️ by <a href="https://youngjuning.js.org" target="_blank">紫竹</a>',
   },
   theme: {
     '@c-primary': '#13aa52',
