b705d00c4dde35799e43e72994197fd4e1f17955
紫竹 <luozhu2021@gmail.com>
2023-10-18 00:50:09 +0800

    chore: 删除底部广告

--- .dumi/theme/layouts/DocLayout/index.tsx
+++ .dumi/theme/layouts/DocLayout/index.tsx
@@ -94,14 +94,6 @@ const DocLayout: FC = () => {
         <Content>
           <article>
             {outlet}
-            <Adsense
-              className="adsbygoogle"
-              style={{ display: "block" }}
-              data-ad-client="ca-pub-7029815294762181"
-              data-ad-slot="6412968057"
-              data-ad-format="auto"
-              data-full-width-responsive="true"
-            />
           </article>
           <ContentFooter />
           <Footer />
